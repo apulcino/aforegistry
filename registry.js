d856a91061a8783984a13bb5aa4585dcf20c43dd
pulcino <alfredo.pulcino@afp.com>
2018-08-14 11:55:47 +0200

    Utilisation d'une zone datashared entre les modules (API et Multicast))

--- registry.js
+++ registry.js
@@ -4,26 +4,27 @@ const express = require('express');
 const router = express.Router();
 const MServiceMgr = require('./MServiceMgr');
 const traceMgr = new (require('../library/tracemgr'))('AFORegistry');
-
+const DataShared = require('./DataShared').DataShared;
+DataShared.initialize(traceMgr);
 
 //------------------------------------------------------------------------------
 // Gestionnaire de composants
 //------------------------------------------------------------------------------
-const MServices = new MServiceMgr(traceMgr);
+//const MServices = new MServiceMgr(traceMgr);
 
 //------------------------------------------------------------------------------
 // Demander la liste des composants disponibles
 // http://localhost:5555/registry/list
 //------------------------------------------------------------------------------
 router.get('/list', (req, res) => {
-    res.status(200).json(MServices.listAll());
+    res.status(200).json(DataShared.MServices.listAll());
 })
 //------------------------------------------------------------------------------
 // Réception de la déclaration d'un composant
 // http://localhost:5555/registry/declare/MSType?host=...&port=...&pathname=....
 //------------------------------------------------------------------------------
 router.get('/declare/:MSType', (req, res) => {
-    let ret = MServices.declare(req.protocol, req.params.MSType, req.query.host, req.query.port, req.query.pathname);
+    let ret = DataShared.MServices.declare(req.protocol, req.params.MSType, req.query.host, req.query.port, req.query.pathname);
     if (ret) {
         res.status(200).json({
             isSuccess: true,
--- server.js
+++ server.js
@@ -5,6 +5,8 @@ const constantes = require('../library/constantes');
 const traceMgr = new (require('../library/tracemgr'))('AFORegistry');
 const multicastSender = require('../library/multicastSender');
 const multicastRecver = require('../library/multicastRecver');
+const DataShared = require('./DataShared').DataShared;
+DataShared.initialize(traceMgr);
 
 const port = process.env.PORT || 0;
 
@@ -15,11 +17,20 @@ const server = http.createServer(application);
 server.listen(port, function () {
   var host = constantes.getServerIpAddress();
   var port = server.address().port
-  mcSender.sendAlways(JSON.stringify({ type: constantes.MSMessageTypeEnum.regAnnonce, host: host, port: port }));
+  mcSender.sendAlways(JSON.stringify({ type: constantes.MSMessageTypeEnum.regAnnonce, host: host, port: port }), 10000);
   traceMgr.info("AFORegistry listening at http://%s:%s", host, port)
 });
 
-// const mcRecver = new multicastRecver(constantes.getServerIpAddress(), constantes.MCastAppPort, constantes.MCastAppAddr, (address, port, message) => {
-//   console.log('MCast Msg: From: ' + address + ':' + port + ' - ' + message);
-// });
+//const MServices = new MServiceMgr(traceMgr);
+const mcRecver = new multicastRecver(constantes.getServerIpAddress(), constantes.MCastAppPort, constantes.MCastAppAddr, (address, port, message) => {
+  switch (message.type) {
+    case constantes.MSMessageTypeEnum.compoDeclare:
+      traceMgr.info('MCast Msg: From: ' + address + ':' + port + ' - ' + JSON.stringify(message));
+      DataShared.MServices.declare('http', message.compoType, message.host, message.port, message.pathname);
+      break;
+    default:
+      traceMgr.debug('MCast Msg: From: ' + address + ':' + port + ' - ' + JSON.stringify(message));
+      break;
+  }
+});
 // traceMgr.log('AFORegistry RESTful API server started on: ' + port);
